5883b9a14a474d5b8eec5b3c378ef8bc95b0ffd7
leshchik <leshchik05@gmail.com>
2023-01-13 00:50:38 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <fstream>
+#include "dependencies/include/nlohmann/json.hpp"
+
+int Calculate_legs();
+
+int main()
+{
+    int x;
+    x=Calculate_legs();
+    std::cout <<"Count of legs =  " << x << std::endl;
+}
+
+int  Calculate_legs()
+{
+    nlohmann::json objJson;
+
+    std::fstream fileInput;
+    fileInput.open("animals.json");
+
+    fileInput >> objJson;
+    int chickens, cows, pigs, all_legs;
+    chickens = objJson["chickens"];
+    cows = objJson["cows"];
+    pigs = objJson["pigs"];
+
+    all_legs = (chickens * 2) + (cows * 4) + (pigs * 4);
+    fileInput.close();
+    return all_legs;
+}
